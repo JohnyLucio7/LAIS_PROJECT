3827a51529aec57cdbbbd2b9c98fa89c533af64c
JohnyLucio7 <johnylucio7@gmail.com>
2020-12-17 07:14:05 -0300

    reconstruindo

--- vacina_carteira_digital/vacina_carteira_digital/wsgi.py
+++ vacina_carteira_digital/vacina_carteira_digital/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vacina_carteira_digital.settings')
 
 application = get_wsgi_application()
